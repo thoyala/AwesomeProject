37c3966ccb3d0498c825c5f8c776486a5a0cddc2
thoyala <thoyala@me.com>
2023-02-09 13:01:03 +0700

    Week11

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -5,6 +5,8 @@ import PokemonTab from "./PokemonTab";
 import Book from "../screens/week9/Book";
 import BookDetail from "../screens/week9/BookDetail";
 import BookForm from "../screens/week9/BookForm";
+import Location from "../screens/week11/Location";
+import LocationQuiz from "../screens/week11/LocationQuiz";
 
 const Stack = createStackNavigator();
 
@@ -12,11 +14,13 @@ export default function RootStack() {
   return (
     <Stack.Navigator initialRouteName="BottomTab">
       <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
-      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title: "Pokemon World" }} />
 
       <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
       <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
-      <Stack.Screen name="BookForm" component={BookForm}   />
+      <Stack.Screen name="BookForm" component={BookForm} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
